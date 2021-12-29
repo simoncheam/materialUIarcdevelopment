ff8883f1a96e8ffbd7cb2abd91a0eee5aa091048
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2021-12-28 21:58:57 -0500

    implemented Header and theme customization, intro to basic components

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,11 @@
+import React from 'react'
+// import { Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
     const { children, window } = props;
@@ -9,27 +13,48 @@ function ElevationScroll(props) {
     // will default to window.
     // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-      target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
     });
-  
+
     return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
+        elevation: trigger ? 4 : 0,
     });
-  }
-  
-  
+}
+
+const useStyles = makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: "3em"
+    },
+    logo:{
+        height: "7em"
+    }
+}))
 
 const Header = (props) => {
-    return (
-        <ElevationScroll>
 
-        <AppBar position="fixed">
-            <Toolbar> Arc Dev</Toolbar>
-            
-        </AppBar>
-        </ElevationScroll>
+    // this gives access to toolbarmargin
+    const classes = useStyles()
+
+    return (
+        <React.Fragment>
+
+            <ElevationScroll>
+
+                <AppBar position="fixed">
+                    <Toolbar disableGutters>
+                        <img alt="company logo" className={classes.logo} src={logo}/>
+                        {/* <Typography variant="h3" color="secondary">Arc Development</Typography> */}
+
+                    </Toolbar>
+
+                </AppBar>
+            </ElevationScroll>
+
+            <div className={classes.toolbarMargin} />
+        </React.Fragment>
     )
 }
 
